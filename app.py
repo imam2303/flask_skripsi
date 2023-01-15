f04c3700596e187800c24b2930535f7f899884e2
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-01-15 20:35:11 +0700

    udah jaduin gaes

--- app.py
+++ app.py
@@ -3,34 +3,63 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
-# database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataprediksi.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:admin@172.19.0.2:3306/skripsi"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+db.create_all()
+
 class DataPrediksi(db.Model):
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nama = db.Column(db.String(30), nullable=False)
     alamat = db.Column(db.String(50), nullable=False)
-    jenis_pmks = db.Column(db.Integer(1), nullable=False)
-    hubungan_dlm_keluarga = db.Column(db.Integer(1), nullable=False)
-    jml_tanggungan_kepala_keluarga = db.Colum
+    jenis_pmks = db.Column(db.Integer, nullable=False)
+    hubungan_dlm_keluarga = db.Column(db.Integer, nullable=False)
+    jml_tanggungan_kepala_keluarga = db.Column(db.Integer, nullable=False)
+    pendapatan_keluarga = db.Column(db.Integer, nullable=False)
+    status_rumah = db.Column(db.Integer, nullable=False)
+    pekerjaan = db.Column(db.Integer, nullable=False)
 
 
 @app.route("/")
 def home():
+    print("Konek teu nya ", db.session.execute('SELECT 1'))
     return render_template('prediksi.html')
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
-    fitur = [np.array(int_features)]
+    data = request.form.get
+    fitur = [np.array([
+        int(data("jenis_pmks")),
+        int(data("hubungan_dlm_keluarga")),
+        int(data("jml_tanggungan_kepala_keluarga")),
+        int(data("pendapatan_keluarga")),
+        int(data("status_rumah")),
+        int(data("pekerjaan"))
+    ])]
     prediction = model.predict(fitur)
+    populate_data = DataPrediksi(
+        nama=data("nama"),
+        alamat=data("alamat"),
+        jenis_pmks=data("jenis_pmks"),
+        hubungan_dlm_keluarga=data("hubungan_dlm_keluarga"),
+        jml_tanggungan_kepala_keluarga=data("jml_tanggungan_kepala_keluarga"),
+        pendapatan_keluarga=data("pendapatan_keluarga"),
+        status_rumah=data("status_rumah"),
+        pekerjaan=data("pekerjaan"),
+    )
+
+    print("Ayaan teu euy",populate_data.nama)
+    db.session.add(populate_data)
+    db.session.commit()
+
     return render_template("prediksi.html", prediction_text = "{}".format(prediction))
 
 if __name__ == "__main__":
--- shell.nix
+++ shell.nix
@@ -6,6 +6,31 @@ in pkgs.mkShell {
     pkgs.python310Packages.pip
     pkgs.python310Packages.virtualenv
     pkgs.python310Packages.flask
+    pkgs.python310Packages.click
+    pkgs.python310Packages.cycler
+    pkgs.python310Packages.colorama
+    #pkgs.python310Packages.contourpy
+    pkgs.python310Packages.greenlet
+    pkgs.python310Packages.itsdangerous
+    pkgs.python310Packages.joblib
+    pkgs.python310Packages.kiwisolver
+    pkgs.python310Packages.matplotlib
+    pkgs.python310Packages.numpy
+    pkgs.python310Packages.packaging
+    pkgs.python310Packages.pandas
+    pkgs.python310Packages.pyparsing
+    pkgs.python310Packages.python-dateutil
+    pkgs.python310Packages.pytz
+    pkgs.python310Packages.scikit-learn
+    pkgs.python310Packages.scipy
+    pkgs.python310Packages.seaborn
+    pkgs.python310Packages.six
+    pkgs.python310Packages.sqlalchemy
+    pkgs.python310Packages.flask_sqlalchemy
+    pkgs.python310Packages.threadpoolctl
+    pkgs.python310Packages.werkzeug
+    pkgs.python310Packages.mariadb
+    pkgs.python310Packages.mysqlclient
   ];
   shellHook = ''
     export PIP_PREFIX=$(pwd)/_build/pip_packages
